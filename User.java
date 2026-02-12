16e4162e6d5c268819244525d8885ea6264b5cd6
Phantomg34k <abdielivanmartinezesparza@gmail.com>
2026-02-12 13:50:57 -0600

    primer intento

---
+++ User.java
@@ -0,0 +1,58 @@
+package com.example.demo;
+
+import java.util.List;
+
+public class User {
+
+  private String name;
+  private String email;
+  private String github;
+  private List<String> tools;
+
+  public User() {
+
+  }
+
+  public User(String name, String email, String github, List<String> tools) {
+    setName(name);
+    setEmail(email);
+    setGithub(github);
+    setTools(tools);
+  }
+
+  public void setGithub(String github) {
+    this.github = github;
+  }
+
+  public void setTools(List<String> tools) {
+    this.tools = tools;
+  }
+
+  public List<String> getTools() {
+    return tools;
+  }
+
+  public String getGithub() {
+    return github;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+ 
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+
+}
